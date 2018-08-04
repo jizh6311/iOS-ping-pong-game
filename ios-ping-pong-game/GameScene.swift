c639f8dc17dac8c5095a39b8cba901646c272e9b
jizh6311 <zjwhut2010@hotmail.com>
2018-08-04 17:27:16 -0400

    Add score tp game

--- ios-ping-pong-game/GameScene.swift
+++ ios-ping-pong-game/GameScene.swift
@@ -15,18 +15,45 @@ class GameScene: SKScene {
     var player = SKSpriteNode()
     var enemy = SKSpriteNode()
     
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        
+        startGame()
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         player = self.childNode(withName: "player") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
         
-        ball.physicsBody?.applyImpulse(CGVector(dx:120, dy: 120))
+        ball.physicsBody?.applyImpulse(CGVector(dx:240, dy: 240))
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0;
         border.restitution = 1
         
         self.physicsBody = border
+    }
+    
+    func startGame() {
+        score = [0, 0]
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        
+        ball.position = CGPoint(x:0, y:0)
+        ball.physicsBody?.velocity = CGVector(dx:0, dy:0)
+        
+        if playerWhoWon == player {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx:240, dy: 240))
+        }
+        else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx:-240, dy: -240))
+        }
+        
+        //TODO: Add the score to UI
+        print(score)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,5 +76,12 @@ class GameScene: SKScene {
         // Called before each frame is rendered
         
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y < player.position.y - 30 {
+            addScore(playerWhoWon: enemy)
+        }
+        else if ball.position.y >= enemy.position.y + 30 {
+            addScore(playerWhoWon: player)
+        }
     }
 }
